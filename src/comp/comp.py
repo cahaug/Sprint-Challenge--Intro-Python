146d9f5c9faff21f9b13db0fb5796b275e692805
cahaug <caleahaug@gmail.com>
2020-02-23 18:45:21 -0700

    repaired comprehensions

--- src/comp/comp.py
+++ src/comp/comp.py
@@ -24,77 +24,77 @@ humans = [
 # Write a list comprehension that creates a list of names of everyone
 # whose name starts with 'D':
 print("Starts with D:")
-a = []
-for human in humans:
-    firstInitial = human.name[0]
-    # print(firstInitial)
-    if firstInitial[0] == "D":
-        a.append(human.name)
-    else:
-        pass
+a = [human.name for human in humans if human.name[0] == 'D']
+# for human in humans:
+#     firstInitial = human.name[0]
+#     # print(firstInitial)
+#     if firstInitial[0] == "D":
+#         a.append(human.name)
+#     else:
+#         pass
 print(a)
 
 # Write a list comprehension that creates a list of names of everyone
 # whose name ends in "e".
 print("Ends with e:")
-b = []
-for human in humans:
-    nameLength = len(human.name)
-    # print(nameLength)
-    lastLetter = human.name[nameLength-1]
-    # print(lastLetter)
-    if lastLetter == "e":
-        b.append(human.name)
-    else:
-        pass
+b = [human.name for human in humans if human.name.endswith("e")]
+# for human in humans:
+#     nameLength = len(human.name)
+#     # print(nameLength)
+#     lastLetter = human.name[nameLength-1]
+#     # print(lastLetter)
+#     if lastLetter == "e":
+#         b.append(human.name)
+#     else:
+#         pass
 print(b)
 
 # Write a list comprehension that creates a list of names of everyone
 # whose name starts with any letter between 'C' and 'G' inclusive.
 print("Starts between C and G, inclusive:")
-c = []
-for human in humans:
-    firstInitial = human.name[0]
-    # print(firstInitial)
-    if firstInitial[0] == "C" or firstInitial[0] == "D" or firstInitial[0] == "E" or firstInitial[0] == "F" or firstInitial[0] == "G":
-        c.append(human.name)
-    else:
-        pass
+c = [human.name for human in humans if human.name[0] == 'C' or human.name[0] == "D" or human.name[0] == "E" or human.name[0] == "F" or human.name[0] == "G"]
+# for human in humans:
+#     firstInitial = human.name[0]
+#     # print(firstInitial)
+#     if firstInitial[0] == "C" or firstInitial[0] == "D" or firstInitial[0] == "E" or firstInitial[0] == "F" or firstInitial[0] == "G":
+#         c.append(human.name)
+#     else:
+#         pass
 print(c)
 
 # Write a list comprehension that creates a list of all the ages plus 10.
 print("Ages plus 10:")
-d = []
-for human in humans:
-    d.append(human.age + 10)
+d = [human.age + 10 for human in humans]
+# for human in humans:
+#     d.append(human.age + 10)
 print(d)
 
 # Write a list comprehension that creates a list of strings which are the name
 # joined to the age with a hyphen, for example "David-31", for all humans.
 print("Name hyphen age:")
-e = []
-for human in humans:
-    e.append(human.name+"-"+str(human.age))
+e = [human.name+"-"+str(human.age) for human in humans]
+# for human in humans:
+#     e.append(human.name+"-"+str(human.age))
 print(e)
 
 # Write a list comprehension that creates a list of tuples containing name and
 # age, for example ("David", 31), for everyone between the ages of 27 and 32,
 # inclusive.
 print("Names and ages between 27 and 32:")
-f = []
-for human in humans:
-    if human.age > 27 and human.age <= 32:
-        print(human.name)
-        f.append((human.name,human.age))
-print(f)
+f = [(human.name, human.age) for human in humans if human.age > 27 and human.age <= 32]
+# for human in humans:
+#     if human.age > 27 and human.age <= 32:
+#         print(human.name)
+#         f.append((human.name,human.age))
+# print(f)
 
 # Write a list comprehension that creates a list of new Humans like the old
 # list, except with all the names uppercase and the ages with 5 added to them.
 # The "humans" list should be unmodified.
 print("All names uppercase:")
-g = []
-for human in humans:
-    g.append(Human(human.name.upper(), human.age+5))
+g = [Human(human.name.upper(), human.age+5) for human in humans]
+# for human in humans:
+#     g.append(Human(human.name.upper(), human.age+5))
 print(g)
 
 
